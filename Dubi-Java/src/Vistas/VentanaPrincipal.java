d084ca892997601cdfb14efb89a60b8a62d242c2
kespinozad <38926010+kespinozad@users.noreply.github.com>
2018-05-27 20:37:40 -0500

    login, principal y producto mejorados

--- Dubi-Java/src/Vistas/VentanaPrincipal.java
+++ Dubi-Java/src/Vistas/VentanaPrincipal.java
@@ -33,6 +33,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         fondo.setVisible(true);
         //FondoPanel.add(fondo);
         FondoPanel.setSize(900, 620);
+        FondoPanel.repaint();
     }
 
     /**
